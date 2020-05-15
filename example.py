759908cfb3d77511c27e0d3bd4a6a359846f48cf
tmarenko <nkrayner@gmail.com>
2020-05-15 18:01:17 +0300

    squad battles: example

--- example.py
+++ example.py
@@ -7,6 +7,7 @@ from lib.coop import CoopPlay
 from lib.timeline import TimelineBattle
 from lib.invasion import WorldBossInvasion
 from lib.routines import DailyMissions, DailyTrivia, ShieldLab
+from lib.squad_battles import SquadBattles
 from lib.game import Game
 import time
 from lib.functions import sleep
@@ -36,6 +37,9 @@ if __name__ == '__main__':
     cp = CoopPlay(game).do_missions()
     tb = TimelineBattle(game).do_missions()
     lb = LegendaryBattle(game).do_missions()
+    # Available: SquadBattles.MODE.DAILY_RANDOM and SquadBattles.MODE.ALL_BATTLES
+    sb = SquadBattles(game).do_missions(mode=SquadBattles.MODE.DAILY_RANDOM)
+
     # Available: MemoryMission.MORDO, MemoryMission.WONG, MemoryMission.ANCIENT_ONE, MemoryMission.KAECILIUS,
     # MemoryMission.DIFFICULTY
     # Use MemoryMission.DIFFICULTY.STAGE_1 up to MemoryMission.DIFFICULTY.STAGE_6 for difficulty
