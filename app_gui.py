4c7728b02bd63410bc41fcd734f74ee108c9f98f
tmarenko <nkrayner@gmail.com>
2020-09-08 19:13:20 +0300

    updater added

--- app_gui.py
+++ app_gui.py
@@ -1,6 +1,8 @@
-﻿import sys
+﻿import os
+import sys
 from PyQt5 import QtWidgets
 from lib.gui.widgets.main import MainWindow
+from lib.updater import Updater
 
 
 def main():
@@ -10,5 +12,13 @@ def main():
     app.exec_()
 
 
+def check_updates():
+    updater = Updater()
+    if updater.check_updates():
+        updater.update_from_new_version()
+        os.execl(sys.executable, sys.executable, *sys.argv)
+
+
 if __name__ == '__main__':
+    check_updates()
     main()
---
+++ lib/updater.py
@@ -0,0 +1,125 @@
+﻿import os
+import shutil
+import lib.logger as logging
+import zipfile
+import importlib
+import importlib.util
+import urllib.request as request
+import version as old_version_module
+from distutils.version import StrictVersion
+from distutils.dir_util import copy_tree
+
+logger = logging.get_logger(__name__)
+
+
+class Version:
+    """Class for working with project versions."""
+
+    @staticmethod
+    def version_to_str(self):
+        """Override __str__ function of StrictVersion to always return full version string."""
+        vstring = '.'.join(map(str, self.version))
+        if self.prerelease:
+            vstring = vstring + self.prerelease[0] + str(self.prerelease[1])
+        return vstring
+
+    def __init__(self, version_module):
+        """Class initialization.
+
+        :param version_module: module of version.py
+        """
+        StrictVersion.__str__ = self.version_to_str
+        self.mff_auto = StrictVersion(version_module.mff_auto)
+        self.mff = StrictVersion(version_module.mff)
+        self.updater = StrictVersion(version_module.updater)
+
+
+class Updater:
+    """Class for working with project updates."""
+
+    NEW_VERSION_FILE = "new_version.py"
+    GITHUB_VERSION_LINK = "https://raw.githubusercontent.com/tmarenko/mff_auto/master/version.py"
+    GITHUB_SOURCE_CODE_URL = "https://github.com/tmarenko/mff_auto/archive/{version}.zip"
+    SOURCE_CODE_FOLDER_NAME = "mff_auto-{version}"
+
+    def __init__(self):
+        """Class initialization."""
+        self.update_source = None
+        self.source_folder = None
+        self.new_version_file = None
+        self.new_version_module = None
+        self.new_version = None
+        self.old_version_module = old_version_module
+        self.old_version = Version(version_module=self.old_version_module)
+        logger.info(f"Loaded version: mff_auto={self.old_version.mff_auto}, game={self.old_version.mff}, "
+                    f"updater={self.old_version.updater}")
+
+    @staticmethod
+    def import_version_file(version_file_path):
+        """Import version file as module.
+
+        :param version_file_path: path to version file.
+
+        :return: imported module.
+        """
+        spec = importlib.util.spec_from_file_location("version", version_file_path)
+        version_module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(version_module)
+        return version_module
+
+    def download_version_file(self):
+        """Download version file from GitHub sources."""
+        with open(self.NEW_VERSION_FILE, "wb") as file:
+            with request.urlopen(self.GITHUB_VERSION_LINK) as url_file:
+                content = url_file.read()
+                file.write(content)
+                return file.name
+
+    def check_updates(self):
+        """Check updates in GitHub.
+
+        :return: True or False: was new version found or not.
+        """
+        self.new_version_file = self.download_version_file()
+        self.new_version_module = self.import_version_file(version_file_path=self.new_version_file)
+        self.new_version = Version(version_module=self.new_version_module)
+        if self.new_version.updater > self.old_version.updater:
+            logger.info(f"Found new version of Updater {self.new_version.updater}, cannot update. "
+                        f"Download last release from project homepage.")
+            return False
+        if self.new_version.mff_auto > self.old_version.mff_auto:
+            logger.info(f"Found new version {self.new_version.mff_auto} for MFF {self.new_version.mff}.")
+            return True
+        else:
+            logger.info(f"Current version {self.new_version.mff_auto} is up to date for MFF {self.new_version.mff}.")
+            return False
+
+    def update_from_new_version(self):
+        """Update project files from new version."""
+        self.update_source = self.download_source_code(self.new_version.mff_auto)
+        logger.info(f"Extracting zip-archive with {self.new_version.mff_auto} version.")
+        with zipfile.ZipFile(self.update_source, 'r') as zip_ref:
+            zip_ref.extractall()
+        self.source_folder = self.SOURCE_CODE_FOLDER_NAME.format(version=self.new_version.mff_auto)
+        logger.info(f"Updating files...")
+        copy_tree(src=self.source_folder, dst="")
+        self.clean()
+
+    def download_source_code(self, version):
+        """Download source code from GitHub by version tag."""
+        download_url = self.GITHUB_SOURCE_CODE_URL.format(version=version)
+        logger.info(f"Downloading new version from URL: {download_url}")
+        with open(f"{version}.zip", "wb") as file:
+            with request.urlopen(download_url) as url_file:
+                content = url_file.read()
+                file.write(content)
+                return file.name
+
+    def clean(self):
+        """Clean all files after updating."""
+        if self.new_version_file:
+            os.remove(self.new_version_file)
+        if self.update_source:
+            os.remove(self.update_source)
+        if self.source_folder:
+            shutil.rmtree(self.source_folder)
