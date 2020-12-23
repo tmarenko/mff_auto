9ac8744c18f6822976fcdd5b400063f694a73781
tmarenko <nkrayner@gmail.com>
2020-12-23 04:44:32 +0300

    timeline battles: skip Honor Token notification

--- lib/game/missions/timeline.py
+++ lib/game/missions/timeline.py
@@ -72,6 +72,9 @@ class TimelineBattle(Missions):
             self.player.click_button(self.ui['TL_REPEAT_TOGGLE'].button)
         self.select_team()
         self.player.click_button(self.ui['TL_SEARCH_BUTTON'].button)
+        if wait_until(self.player.is_ui_element_on_screen, timeout=3, ui_element=self.ui['TL_HONOR_TOKENS_LIMIT']):
+            logger.debug(f"Your Honor Tokens is more than 60000. Can't acquire more but still playing battle.")
+            self.player.click_button(self.ui['TL_HONOR_TOKENS_LIMIT'].button)
         if wait_until(self.player.is_ui_element_on_screen, timeout=3, ui_element=self.ui['INVENTORY_FULL']):
             logger.warning(f"Your inventory is full, cannot start battle.")
             self.player.click_button(self.ui['INVENTORY_FULL'].button)
