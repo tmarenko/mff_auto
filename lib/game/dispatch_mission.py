784b9708874fe4e7d7535dcb07257c417afa3f45
tmarenko <nkrayner@gmail.com>
2021-08-04 10:30:44 +0300

    dispatch mission: acquire next reward

--- lib/game/dispatch_mission.py
+++ lib/game/dispatch_mission.py
@@ -24,8 +24,7 @@ class DispatchMission:
         if self.emulator.is_ui_element_on_screen(ui_element=self.ui['DISPATCH_SECTOR_BUTTON']):
             logger.debug("Closing DISPATCH sector menu")
             self.emulator.click_button(self.ui['DISPATCH_SECTOR_CLOSE'].button)
-        if self._acquire_sectors_from_the_left():
-            self._acquire_sectors_from_the_right()
+        self._acquire_rewards_from_sectors()
         self.game.go_to_main_menu()
 
     def _drag_to_the_left(self):
@@ -38,42 +37,15 @@ class DispatchMission:
                            self.ui['DISPATCH_DRAG_RIGHT_POSITION'].button)
         r_sleep(1)
 
-    def _drag_to_the_right(self):
-        """Drag Dispatch sector menu to the right side."""
-        logger.debug("Dragging to the right side.")
-        self.emulator.drag(self.ui['DISPATCH_DRAG_RIGHT_POSITION'].button,
-                           self.ui['DISPATCH_DRAG_LEFT_POSITION'].button)
-        r_sleep(1)
-        self.emulator.drag(self.ui['DISPATCH_DRAG_RIGHT_POSITION'].button,
-                           self.ui['DISPATCH_DRAG_LEFT_POSITION'].button)
-        r_sleep(1)
-
-    def _acquire_sectors_from_the_left(self):
-        """Acquire all available rewards in sectors from the left side.
-
-        :return: True or False: were all dispatch missions closed on the left side.
-        """
+    def _acquire_rewards_from_sectors(self):
         self._drag_to_the_left()
-        dispatch_closed = []
-        for sector_index in range(1, 7):
-            sector_ui = self.ui[f'DISPATCH_ACQUIRE_SECTOR_{sector_index}']
-            if self.emulator.is_ui_element_on_screen(ui_element=sector_ui):
-                self.emulator.click_button(sector_ui.button)
-                if wait_until(self.emulator.is_ui_element_on_screen, timeout=3,
-                              ui_element=self.ui['DISPATCH_ACQUIRE_OK_BUTTON']):
-                    logger.debug(f"Acquired rewards for sector #{sector_index}")
-                    self.emulator.click_button(self.ui['DISPATCH_ACQUIRE_OK_BUTTON'].button)
-                    dispatch_closed.append(True)
-        return len(dispatch_closed) == 6
-
-    def _acquire_sectors_from_the_right(self):
-        """Acquire all available rewards in sectors from the right side."""
-        self._drag_to_the_right()
-        for sector_index in range(6, 11):
-            sector_ui = self.ui[f'DISPATCH_ACQUIRE_SECTOR_{sector_index}']
-            if self.emulator.is_ui_element_on_screen(ui_element=sector_ui):
-                self.emulator.click_button(sector_ui.button)
-                if wait_until(self.emulator.is_ui_element_on_screen, timeout=3,
-                              ui_element=self.ui['DISPATCH_ACQUIRE_OK_BUTTON']):
-                    logger.debug(f"Acquired rewards for sector #{sector_index}")
-                    self.emulator.click_button(self.ui['DISPATCH_ACQUIRE_OK_BUTTON'].button)
+        if self.emulator.is_ui_element_on_screen(ui_element=self.ui['DISPATCH_ACQUIRE_SECTOR_1']):
+            self.emulator.click_button(self.ui['DISPATCH_ACQUIRE_SECTOR_1'].button)
+            logger.debug("Started acquiring rewards from sectors.")
+            while not self.emulator.is_ui_element_on_screen(ui_element=self.ui['DISPATCH_ACQUIRE_OK_BUTTON']):
+                if self.emulator.is_ui_element_on_screen(ui_element=self.ui['DISPATCH_NEXT_REWARD']):
+                    logger.debug("Next reward is available, acquiring.")
+                    self.emulator.click_button(self.ui['DISPATCH_NEXT_REWARD'].button)
+                r_sleep(0.3)  # In order to not be fast as Sonic
+            logger.debug("Acquired all available rewards.")
+            self.emulator.click_button(self.ui['DISPATCH_ACQUIRE_OK_BUTTON'].button)
